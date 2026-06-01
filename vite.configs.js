1732afad9db776c73845320a34b5fa174f04638a
Orr Krupnik <orr@hirundo.io>
2026-06-01 15:32:57 +0300

    fix github pages build base path

--- vite.configs.js
+++ vite.configs.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/world-cup-predictions/"
+  base: "/world-cup-predictions/",
+  plugins: [react()]
 });
